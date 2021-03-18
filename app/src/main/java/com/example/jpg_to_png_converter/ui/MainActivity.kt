46a692812504d9204749c94aafb29881a4235bc4
FatED42 <cjkrol42@gmail.com>
2021-03-18 19:47:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/jpg_to_png_converter/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jpg_to_png_converter.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jpg_to_png_converter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
